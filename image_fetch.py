b98e4be845154d199b4acf8baf3c9f730032373e
alkshmir <shirakam.itsuk252@gmail.com>
2022-06-11 22:27:39 +0900

    bugfix

--- image_fetch.py
+++ image_fetch.py
@@ -5,53 +5,58 @@ import os
 args = sys.argv
 
 # 同じファイル名が存在するかチェック
-def duplicate_rename(value, count=1):
-    if os.path.exists(value):
-        # 存在した場合
+def duplicate_rename(value, already_written_files, count=1):
+    # フルパスから「フルパスタイトル」と「拡張子」を分割
+    ftitle, fext = os.path.splitext(value)
+    m = re.match(r'(.+)_(\d+?)$', ftitle)
+    # if matched
+    if m is not None:
+        #print(m[2])
+        count = int(m[2]) + 1
+    
+    # タイトル末尾に_(数値)が在れば削除する。
+    newftitle = re.sub(r'_\d+?$', "", ftitle)
 
-        # フルパスから「フルパスタイトル」と「拡張子」を分割
-        ftitle, fext = os.path.splitext(value)
-
-        # タイトル末尾に(数値)が在れば削除する。
-        newftitle = re.sub(r'_\d+?$', "", ftitle)
-
+    while(value in already_written_files):        
         # _1 という文字列を作成
         addPara = '_' + '{}'.format(count)
 
         # フルパスタイトル + _1 + 拡張子のファイル名を作成
-        fpath = os.path.join(newftitle + addPara + fext)
+        value = os.path.join(newftitle + addPara + fext)
 
         # リネームしたファイルを表示
-        print('Rename: %s' % fpath)
+        #print('Rename: %s' % value)
 
-        # 再度渡してリネームしたファイル名が存在しないかチェック
-        return (duplicate_rename(fpath, count + 1))
-    else:
-        # 存在しない場合
-        return value
+        count += 1
+    return value
 
 if __name__ == "__main__":
     in_html = args[1]
 
-    dir = args[1][:-5]
+    backup_dir = 'backuped'
+    image_dir = backup_dir + '/' + os.path.splitext(os.path.basename(in_html))[0]
 
-    os.makedirs(dir, exist_ok=True)
+    os.makedirs(image_dir, exist_ok=True)
     with open(in_html, 'r') as f:
         s = f.read()
         
         matched = re.findall(r'src=\"(https?://cdn.discordapp\.com/attachments/.+\.(jpg|png|JPG|PNG))\"', s)
 
         urls = [m[0] for m in matched]
-        filenames = [re.match(".+/(.+?)([\?#;].*)?$", url)[1] for url in urls]
-        new_html_filename = os.path.splitext(os.path.basename(in_html))[0] + '_bak.html'
+        img_filenames = [re.match(".+/(.+?)([\?#;].*)?$", url)[1] for url in urls]
+        new_html_filename = backup_dir + '/' + os.path.splitext(os.path.basename(in_html))[0] + '_bak.html'
         new_html = s
 
-        for fn, url in zip(filenames, urls):
-            path = dir + '/' + fn
-            path = duplicate_rename(path)
-            os.system("wget {} -O '{}'".format(url, path))
+        already_written_files = []
+        for fn, url in zip(img_filenames, urls):
+            path = image_dir + '/' + fn
+            path = duplicate_rename(path, already_written_files)
+            already_written_files.append(path)
+            #os.system("wget {} -O '{}'".format(url, path))
 
-            new_html = new_html.replace(url, path)
+            # htmlから見た画像ディレクトリの場所
+            image_path_from_html = os.path.splitext(os.path.basename(in_html))[0] + '/' + os.path.basename(path)
+            new_html = new_html.replace(url, image_path_from_html)
 
     # write new html
     with open(new_html_filename, 'w') as f_new:
